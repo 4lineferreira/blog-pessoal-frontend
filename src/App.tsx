2ba1790c5588e208e014eaa0f057650c277cb468
4lineferreira <alineferreirasnsd@gmail.com>
2025-01-15 12:24:11 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './pages/home/Home'
+import Home from './pages/home/Home'
+
+function App() {
+  return (
+    <Home/>
+  )
+
+}
+
+export default App

854d7f6d7c1f60802070a15dc6d68f24da7895c3
4lineferreira <alineferreirasnsd@gmail.com>
2025-01-20 09:14:23 -0300

    fix: correção no link do swagger

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-        baseURL: 'https://blogpessoalbackend-yyjt.onrender.com/swagger-ui/index.html'
+        baseURL: 'https://blogpessoalbackend-yyjt.onrender.com'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) =>{
